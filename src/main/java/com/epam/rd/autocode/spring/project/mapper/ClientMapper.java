a435a6b812f870a311e0adc7e613702b40449da0
Vitalii Verzun <vitaliy.vz.main@gmail.com>
2026-01-21 19:35:49 +0200

    feat: add ClientMapper and GlobalMapperConfig

---
+++ src/main/java/com/epam/rd/autocode/spring/project/mapper/ClientMapper.java
@@ -0,0 +1,12 @@
+package com.epam.rd.autocode.spring.project.mapper;
+
+import com.epam.rd.autocode.spring.project.conf.GlobalMapperConfig;
+import com.epam.rd.autocode.spring.project.dto.ClientDTO;
+import com.epam.rd.autocode.spring.project.model.Client;
+import org.mapstruct.Mapper;
+
+@Mapper(config = GlobalMapperConfig.class)
+public interface ClientMapper {
+
+    ClientDTO entityToDto(Client entity);
+}
