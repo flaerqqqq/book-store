62c622880dce5deb37b5c4ec46bd25d91052335d
Vitalii Verzun <vitaliy.vz.main@gmail.com>
2026-01-23 23:13:16 +0200

    feat: add models for Order and BookItem

--- src/main/java/com/epam/rd/autocode/spring/project/dto/BookItemDTO.java
+++ src/main/java/com/epam/rd/autocode/spring/project/dto/BookItemDTO.java
@@ -1,5 +1,17 @@
 package com.epam.rd.autocode.spring.project.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookItemDTO {
-    // TODO Place your code here
+
+    private String bookName;
+
+    private Integer quantity;
 }
--- src/main/java/com/epam/rd/autocode/spring/project/model/Order.java
+++ src/main/java/com/epam/rd/autocode/spring/project/model/Order.java
@@ -1,5 +1,45 @@
 package com.epam.rd.autocode.spring.project.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Order {
-    // TODO Place your code here
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookItem> bookItems = new ArrayList<>();
+
+    public void addBookItem(BookItem bookItem) {
+        this.bookItems.add(bookItem);
+        bookItem.setOrder(this);
+    }
 }
