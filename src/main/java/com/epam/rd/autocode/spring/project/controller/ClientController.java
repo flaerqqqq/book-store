5c01019f5337f6a2f6cae3c66ad07e8a62d3f678
Vitalii Verzun <vitaliy.vz.main@gmail.com>
2026-01-28 23:48:22 +0200

    refactor: update URI mappings for login and register pages for client

--- src/main/java/com/epam/rd/autocode/spring/project/controller/ClientController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/ClientController.java
@@ -12,10 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/clients")
 @RequiredArgsConstructor
 public class ClientController {
 
